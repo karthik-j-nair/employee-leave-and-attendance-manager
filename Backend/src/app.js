fb1853275ca060c71f83016c551f98172dfa259f
Karthik J Nair <you@example.com>
2026-04-21 21:15:45 +0530

    run build

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,10 +12,14 @@ const app = express();
  */
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
+app.use(express.static("./public"));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }));
+}
 
 /**
  * Routes
