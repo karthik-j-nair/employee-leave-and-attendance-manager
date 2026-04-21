8609d24039a068904a6d53164f852d5db825cbf4
Karthik J Nair <you@example.com>
2026-04-21 22:17:06 +0530

    some bug fix

--- Frontend/src/features/home/components/UserAttendanceModal.jsx
+++ Frontend/src/features/home/components/UserAttendanceModal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
